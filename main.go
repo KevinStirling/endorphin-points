7d25e29c7953f8044105dea3b9f3e57c60d16e40
KevinStirling <youngks93@gmail.com>
2023-05-30 22:23:30 -0400

    add test command

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/bwmarrin/discordgo"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
@@ -15,9 +16,19 @@ func main() {
 		fmt.Printf("Error while starting bot: %s", err)
 		return
 	}
+	// create command
+	cmd := &discordgo.ApplicationCommand{Name: "test"}
+	_, err = dg.ApplicationCommandCreate(os.Getenv("APP_ID"), "", cmd)
+	if err != nil {
+		fmt.Printf("Could not create command: %s ", err)
+		return
+	}
 
 	// Add the message handler
 	dg.AddHandler(messageCreate)
+
+	// Add the test command handler
+	dg.AddHandler(commandCreate)
 
 	// Add the Guild Messages intent
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
@@ -28,7 +39,6 @@ func main() {
 		fmt.Printf("Error while connecting to gateway: %s", err)
 		return
 	}
-
 	// Wait until Ctrl+C or another signal is received
 	fmt.Println("The bot is now running. Press Ctrl+C to exit.")
 	sc := make(chan os.Signal, 1)
@@ -37,6 +47,26 @@ func main() {
 
 	// Close the Discord session
 	dg.Close()
+}
+
+func commandCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
+
+	data := i.ApplicationCommandData()
+	switch data.Name {
+	case "test":
+		fmt.Println("Hit test command")
+		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Shut Up",
+			},
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	return
+
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
